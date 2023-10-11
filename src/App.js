e7d0d0c3eedd1c84d96303db3006e1cf806363bf
Inna18 <kinvdev21@gmail.com>
2023-10-11 09:43:18 +0900

    add matching properties and set to true if two cards are identical

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { "src": "./img/helmet-1.png" },
-  { "src": "./img/potion-1.png" },
-  { "src": "./img/ring-1.png" },
-  { "src": "./img/scroll-1.png" },
-  { "src": "./img/shield-1.png" },
-  { "src": "./img/sword-1.png" },
+  { "src": "./img/helmet-1.png", matched: false },
+  { "src": "./img/potion-1.png", matched: false },
+  { "src": "./img/ring-1.png", matched: false },
+  { "src": "./img/scroll-1.png", matched: false },
+  { "src": "./img/shield-1.png", matched: false },
+  { "src": "./img/sword-1.png", matched: false },
 ]
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffleCards
   const shuffleCards = () => {
@@ -25,6 +28,42 @@ function App() {
     setTurns(0);
   }
 
+  // make a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  }
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("match!");
+        setCards(prevState => {
+          return prevState.map(card => {
+            if (card.src === choiceOne.src || card.src === choiceTwo.src) {
+              return {...card, matched: true}
+            } else {
+              return card;
+            }
+          })
+        });
+        resetTurn();
+      } else {
+        console.log("don't match!");
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
+  // reset turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(prevState => prevState + 1);
+  }
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -32,7 +71,7 @@ function App() {
 
       <div className="card-grid">
         {cards.map(card => (
-          <SingleCard key={card.id} card={card}/>
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice}/>
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,10 +1,15 @@
 import "./SingleCard.css";
 
-export default function SingleCard({card}) {
+export default function SingleCard({card, handleChoice}) {
+
+  const handleClick = (card) => {
+    handleChoice(card);
+  }
+
   return (
     <div className="card">
       <img className="front" src={card.src} alt="front" />
-      <img className="back" src="./img/cover.png" alt="back" />
+      <img className="back" src="./img/cover.png" alt="back" onClick={() => handleClick(card)}/>
     </div>
   )
 }
